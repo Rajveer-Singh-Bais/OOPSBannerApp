af7646c436f5471a1675d360544ec904541698c5
Rajveer-Singh-Bais <rs1127@srmist.edu.in>
2026-02-20 11:01:09 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -6,12 +6,12 @@ public class OOPSBannerApp {
 
         // 2. Populate the array using String.join to construct each line
         banner[0] = String.join("", "  *** ", "   *** ", " ***** ", "  **** ");
-        banner[1] = String.join("", " * * ", "  * * ", " * * ", " * ");
-        banner[2] = String.join("", " * * ", "  * * ", " * * ", " * ");
-        banner[3] = String.join("", " * * ", "  * * ", " ***** ", "  *** ");
-        banner[4] = String.join("", " * * ", "  * * ", " * ", "     * ");
-        banner[5] = String.join("", " * * ", "  * * ", " * ", "     * ");
-        banner[6] = String.join("", "  *** ", "   *** ", " * ", " **** ");
+        banner[1] = String.join("", " *   * ", " *   * ", "*    * ", " * ");
+        banner[2] = String.join("", " *   * ", " *   * ", "*    * ", " * ");
+        banner[3] = String.join("", " *   * ", " *   * ", "***** ", "   *** ");
+        banner[4] = String.join("", " *   * ", " *   * ", "* ", "          * ");
+        banner[5] = String.join("", " *   * ", " *   * ", "* ", "          * ");
+        banner[6] = String.join("", "  *** ", "   *** ", " * ", "      **** ");
 
         // 3. Use an Enhanced For-Loop to iterate through the array and print each line
         for (String line : banner) {
