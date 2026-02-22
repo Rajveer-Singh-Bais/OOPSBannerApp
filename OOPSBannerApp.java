92ab1ed8d1c0ea6812b835b5f63d59d9eb1e4da9
Rajveer-Singh-Bais <rs1127@srmist.edu.in>
2026-02-22 22:25:56 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,11 +2,11 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
         System.out.println("  *** " + "   *** " + " ***** " + "  **** ");
-        System.out.println(" * *" + "  * *" + " * *" + " * ");
-        System.out.println(" * *" + "  * *" + " * *" + " * ");
-        System.out.println(" * *" + "  * *" + " ***** " + "  *** ");
-        System.out.println(" * *" + "  * *" + " * " + "     *");
-        System.out.println(" * *" + "  * *" + " * " + "     *");
-        System.out.println("  *** " + "   *** " + " * " + " **** ");
+        System.out.println(" *   *" + "  *   *" + " *    *" + " * ");
+        System.out.println(" *   *" + "  *   *" + " *    *" + " * ");
+        System.out.println(" *   *" + "  *   *" + " ***** " + "  *** ");
+        System.out.println(" *   *" + "  *   *" + " * " + "          *");
+        System.out.println(" *   *" + "  *   *" + " * " + "          *");
+        System.out.println("  *** " + "   *** " + " * " + "      **** ");
     }
 }
