e920bcbdf1679d6f028fb455ca858152cfa491f2
Rajveer Singh Bais <rs1127@srmist.edu.in>
2026-02-22 21:37:23 +0530

    Initial commit

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // UC3 Goal: Use String.join() with an empty delimiter "" [cite: 158, 161]
+        System.out.println(String.join("", "  *** ", "   *** ", " ***** ", "  **** "));
+        System.out.println(String.join("", " *   * ", " *   * ", "*   * ", " * "));
+        System.out.println(String.join("", " *   * ", " *   * ", "*   * ", " * "));
+        System.out.println(String.join("", " *   * ", " *   * ", "***** ", "  *** "));
+        System.out.println(String.join("", " *   * ", " *   * ", "* ", "         * "));
+        System.out.println(String.join("", " *   * ", " *   * ", "* ", "         * "));
+        System.out.println(String.join("", "  *** ", "   *** ", " * ", "     **** "));
+    }
+}
