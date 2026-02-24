9189b425a1fd6263032b1233fe62468322257b3a
Rajveer Singh Bais <rs1127@srmist.edu.in>
2026-02-24 13:10:47 +0530

    Added UC8

---
+++ UC8.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8 {
+    public static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        map.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    "
+        });
+
+        map.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                " ****"
+        });
+
+        return map;
+    }
+    public static void renderBanner(String text, Map<Character, String[]> map) {
+
+        if (text == null || text.isEmpty()) {
+            System.out.println("No text provided.");
+            return;
+        }
+
+        int rows = map.get(text.charAt(0)).length;
+
+        for (int i = 0; i < rows; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : text.toCharArray()) {
+
+                String[] pattern = map.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append(" ");
+                } else {
+                    line.append("???? ").append(" ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    /*
+     * Main Method
+     */
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patterns = buildPatternMap();
+
+        String word = "OOPS";
+
+        renderBanner(word, patterns);
+    }
+}
